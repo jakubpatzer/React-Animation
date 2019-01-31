ab444f3eae6389ace95438e9572c8edd15e5e960
jakubpatzer <jakub.patzer@protonmail.com>
2019-01-31 14:32:39 +0100

    add spring to component1

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Spring } from 'react-spring';
 
 export default function Component1() {
   return (
-    <div>
-      <h1>Component 1</h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et eros nec eros tempus convallis a eget diam. Mauris pellentesque lorem eros, ac facilisis arcu convallis sollicitudin. Fusce mi purus.</p>
-    </div>
+    <Spring>
+        { props => (
+            <div style={props}>
+                <div style={c1style}>
+                    <h1>Component 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et eros nec eros tempus convallis a eget diam. Mauris pellentesque lorem eros, ac facilisis arcu convallis sollicitudin. Fusce mi purus.</p>
+                </div>
+            </div>
+        ) }
+    </Spring>
+    
   )
+}
+
+const c1style = {
+    background: 'steelblue',
+    color: 'white',
+    padding: '1.5rem'
 }
