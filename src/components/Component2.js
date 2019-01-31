b29a077fef501095789e90dc106ae9a52c178d48
jakubpatzer <jakub.patzer@protonmail.com>
2019-01-31 15:09:08 +0100

    add transition and toggle

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -14,6 +14,7 @@ export class Component2 extends Component {
                 <div style={c2style}>
                     <h1>Component 2</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et eros nec eros tempus convallis a eget diam. Mauris pellentesque lorem eros, ac facilisis arcu convallis sollicitudin. Fusce mi purus.</p>
+                    <button style={btn} onClick={this.props.toggle}>Toggle Component 3</button>
                 </div>
             </div>
         ) }
@@ -26,6 +27,14 @@ const c2style = {
     background: 'slateblue',
     color: 'white',
     padding: '1.5rem'
+}
+const btn = {
+    background: '#333',
+    color: '#fff',
+    padding: '1rem 2rem',
+    border: 'none',
+    textTransform: 'uppercase',
+    margin: '15px 0'
 }
 
 export default Component2
---
+++ src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component3() {
+  return (
+    <div style={c3style}>
+      <h1>Component 3</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et eros nec eros tempus convallis a eget diam. Mauris pellentesque lorem eros, ac facilisis arcu convallis sollicitudin. Fusce mi purus.</p>
+    </div>
+  )
+}
+
+const c3style = {
+    background: 'skyblue',
+    color: 'white',
+    padding: '1.5rem 1.5rem 5rem 1.5rem'
+}
